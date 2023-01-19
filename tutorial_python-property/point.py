0d8a7a62ef4abcb91d69e9a18dd0ba48a3f87b46
Tanay Sood <tanay.sd@gmail.com>
2023-01-19 09:56:48 -0800

    read only attribs using property decorator

--- tutorial_python-property/point.py
+++ tutorial_python-property/point.py
@@ -4,14 +4,10 @@ class Point:
         self._x = x
         self._y = y
 
-    def get_x(self):
+    @property
+    def x(self):
         return self._x
 
-    def set_x(self, value):
-        self._x = value
-
-    def get_y(self):
+    @property
+    def y(self):
         return self._y
-
-    def set_y(self, value):
-        self._y = value
